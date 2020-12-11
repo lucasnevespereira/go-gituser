a63415fca84c88fc57ff1ca147e9badf74b4acaf
lucasnevespereira <pereiraneveslucas@gmail.com>
2020-12-11 15:14:56 +0100

    handled invalid arguments

--- main.go
+++ main.go
@@ -1,3 +1,17 @@
 package main
 
-func main() {}
+import (
+	"errors"
+	"go-gituser/helpers"
+	"os"
+)
+
+var (
+	errInvalidArguments = errors.New("Invalid Arguments 🙁")
+)
+
+func main() {
+	if len(os.Args) != 2 {
+		helpers.PrintError(errInvalidArguments)
+	}
+}

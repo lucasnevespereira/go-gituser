74c5b6538f2bc3c4d196a4d3a1e775afa50652d7
lucasnevespereira <pereiraneveslucas@gmail.com>
2020-12-11 23:15:32 +0100

    improved help text

--- helpers/help.go
+++ helpers/help.go
@@ -10,10 +10,11 @@ func PrintHelp() {
 	fmt.Println("Description:")
 	fmt.Println("This programs automates the git config command.")
 	fmt.Println("There is 3 modes for this program")
-	fmt.Println("w for work account, s for school account, p for personal account")
+	fmt.Println(" - <work> for a professional account \n - <school> for a school account \n - <personal> for a personal account")
 	fmt.Println("")
 	fmt.Println("Usage:")
 	fmt.Println("To use the program you just need to call the executable")
-	fmt.Println("./gituser <mode>")
+	fmt.Println("")
+	fmt.Println(" ./gituser <mode>")
 	fmt.Println("")
 }

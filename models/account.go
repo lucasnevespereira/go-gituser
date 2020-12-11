0c3a91d35409752d4225ee97a3119e3dfca086b8
lucasnevespereira <pereiraneveslucas@gmail.com>
2020-12-11 22:55:34 +0100

    improved account structure

--- models/account.go
+++ models/account.go
@@ -2,34 +2,10 @@ package models
 
 // Account is the account structure
 type Account struct {
-	username string
-	email    string
-}
-
-// SetWorkMode sets work git username and email
-func (a *Account) SetWorkMode() {
-	a.username = "lucas.pereiraneves"
-	a.email = "lucas.pereiraneves@grtgaz.com"
-}
-
-// SetSchoolMode sets school git username and email
-func (a *Account) SetSchoolMode() {
-	a.username = "lucas.nevespereira"
-	a.email = "lucas.nevespereira@eemi.com"
-}
-
-// SetPersonalMode sets personal git username and email
-func (a *Account) SetPersonalMode() {
-	a.username = "lucasnevespereira"
-	a.email = "pereiraneveslucas@gmail.com"
-}
-
-// GetAccountUsername is..
-func (a *Account) GetAccountUsername() string {
-	return a.username
-}
-
-// GetAccountEmail is..
-func (a *Account) GetAccountEmail() string {
-	return a.email
+	PersonalUsername string `json:"personalUsername"`
+	PersonaEmail     string `json:"personalEmail"`
+	WorkUsername     string `json:"workUsername"`
+	WorkEmail        string `json:"workEmail"`
+	SchoolUsername   string `json:"schoolUsername"`
+	SchoolEmail      string `json:"schoolEmail"`
 }

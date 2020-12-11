4cf6d4beb2aed35a5a0972d600df4a11de5c76d3
lucasnevespereira <pereiraneveslucas@gmail.com>
2020-12-11 19:34:03 +0100

    added help flag

---
+++ helpers/help.go
@@ -0,0 +1,8 @@
+package helpers
+
+import "fmt"
+
+// PrintHelp prints help
+func PrintHelp() {
+	fmt.Println("This is help")
+}
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"go-gituser/helpers"
 	"go-gituser/models"
 	"os"
@@ -11,6 +12,12 @@ func main() {
 
 	if len(os.Args) != 2 {
 		helpers.PrintErrorInvalidArguments()
+	}
+
+	help := flag.Bool("help", false, "Print usage information")
+	flag.Parse()
+	if *help {
+		helpers.PrintHelp()
 	}
 
 	argValue := strings.ToUpper(os.Args[1])

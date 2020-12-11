d8f2de444816973c46d6e658bb65206a38487e9f
lucasnevespereira <pereiraneveslucas@gmail.com>
2020-12-11 22:55:57 +0100

    removed unused error logs

--- helpers/error.go
+++ helpers/error.go
@@ -9,6 +9,7 @@ import (
 var (
 	errInvalidArguments = errors.New("Invalid Arguments 🙁")
 	errExecutingMode    = errors.New("Something went wrong executing this mode 😭")
+	errReadingInput     = errors.New("Couldn't understand your input 🤯")
 )
 
 // PrintErrorInvalidArguments is used to handle error logs for invalid arguments
@@ -19,4 +20,9 @@ func PrintErrorInvalidArguments() {
 // PrintErrorExecutingMode is used to handle error logs after execution
 func PrintErrorExecutingMode() {
 	fmt.Fprintf(os.Stderr, "error %v \n", errExecutingMode)
+}
+
+// PrintErrorReadingInput is used to handle error logs reading input
+func PrintErrorReadingInput() {
+	fmt.Fprintf(os.Stderr, "error %v \n", errReadingInput)
 }

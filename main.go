6706ffcd44a258ea156ce1d8b9eb2c8df2cbcad5
lucasnevespereira <pereiraneveslucas@gmail.com>
2020-12-11 15:21:00 +0100

    switching between modes

--- main.go
+++ main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"errors"
+	"fmt"
 	"go-gituser/helpers"
 	"os"
+	"strings"
 )
 
 var (
@@ -14,4 +16,18 @@ func main() {
 	if len(os.Args) != 2 {
 		helpers.PrintError(errInvalidArguments)
 	}
+
+	argValue := strings.ToUpper(os.Args[1])
+
+	switch argValue {
+	case "WORK":
+		fmt.Println("Work mode")
+	case "SCHOOL":
+		fmt.Println("School mode")
+	case "PERSONAL":
+		fmt.Println("Personal mode")
+	default:
+		fmt.Println("Personal Mode")
+	}
+
 }

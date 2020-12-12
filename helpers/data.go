bdfe5b0dba02ff8bf665571e7470333aeecd1762
lucasnevespereira <pereiraneveslucas@gmail.com>
2020-12-12 17:12:19 +0100

    added ReadAccountsData

--- helpers/data.go
+++ helpers/data.go
@@ -2,6 +2,7 @@ package helpers
 
 import (
 	"fmt"
+	"go-gituser/models"
 	"io/ioutil"
 )
 
@@ -12,4 +13,22 @@ func GetDataFromJSON(filename string) ([]byte, error) {
 		fmt.Println(err)
 	}
 	return data, nil
+}
+
+// ReadAccountsData prints accounts infos to the user
+func ReadAccountsData(account models.Account) {
+	fmt.Println("Hello, this is your accounts data 🧑🏻‍💻")
+	fmt.Println("")
+	fmt.Println("Personal Git Account :")
+	fmt.Printf("Username: %v\n", account.PersonalUsername)
+	fmt.Printf("Email: %v\n", account.PersonalEmail)
+	fmt.Println("")
+	fmt.Println("School Git Account :")
+	fmt.Printf("Username: %v\n", account.SchoolUsername)
+	fmt.Printf("Email: %v\n", account.SchoolEmail)
+	fmt.Println("")
+	fmt.Println("Work Git Account :")
+	fmt.Printf("Username: %v\n", account.WorkUsername)
+	fmt.Printf("Email: %v\n", account.WorkUsername)
+	fmt.Println("")
 }

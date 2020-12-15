20f571fce6da266c926112874e87045ae610a02d
lucas.nevespereira <lucas.nevespereira@eemi.com>
2020-12-15 20:29:14 +0100

    improved error logs

--- helpers/error.go
+++ helpers/error.go
@@ -4,6 +4,8 @@ import (
 	"errors"
 	"fmt"
 	"os"
+
+	"github.com/fatih/color"
 )
 
 var (
@@ -14,15 +16,29 @@ var (
 
 // PrintErrorInvalidArguments is used to handle error logs for invalid arguments
 func PrintErrorInvalidArguments() {
-	fmt.Fprintf(os.Stderr, "error: %v \n", errInvalidArguments)
+	fmt.Fprintf(os.Stderr, color.RedString("Error: ")+"%v \n", errInvalidArguments)
+	fmt.Println("For further information see 'gituser --help'")
 }
 
 // PrintErrorExecutingMode is used to handle error logs after execution
 func PrintErrorExecutingMode() {
-	fmt.Fprintf(os.Stderr, "error: %v \n", errExecutingMode)
+	fmt.Fprintf(os.Stderr, color.RedString("Error: ")+"%v \n", errExecutingMode)
 }
 
 // PrintErrorReadingInput is used to handle error logs reading input
 func PrintErrorReadingInput() {
-	fmt.Fprintf(os.Stderr, "error: %v \n", errReadingInput)
+	fmt.Fprintf(os.Stderr, color.RedString("Error: ")+"%v \n", errReadingInput)
+}
+
+// PrintErrorReadingAccount is used to handle error logs reading accounts
+func PrintErrorReadingAccount(mode string) {
+	fmt.Println("")
+	fmt.Printf("You have no %v account! \n", mode)
+	fmt.Println("")
+	color.Cyan("Additional info:")
+	fmt.Printf("To add a %v account you need to add it to data/config.json \n", mode)
+	fmt.Println("Then recompile your program:")
+	fmt.Println("Step 1 : Go to the source directory for this project")
+	fmt.Println("Step 2 : Once there please run go build -o gituser")
+	fmt.Println("")
 }

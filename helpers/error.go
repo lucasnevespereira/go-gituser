fcaa8035f6afcf52895e8fd99e6288cc18f0768d
lucasnevespereira <pereiraneveslucas@gmail.com>
2020-12-15 16:20:26 +0100

    improved error print line

--- helpers/error.go
+++ helpers/error.go
@@ -14,15 +14,15 @@ var (
 
 // PrintErrorInvalidArguments is used to handle error logs for invalid arguments
 func PrintErrorInvalidArguments() {
-	fmt.Fprintf(os.Stderr, "error %v \n", errInvalidArguments)
+	fmt.Fprintf(os.Stderr, "error: %v \n", errInvalidArguments)
 }
 
 // PrintErrorExecutingMode is used to handle error logs after execution
 func PrintErrorExecutingMode() {
-	fmt.Fprintf(os.Stderr, "error %v \n", errExecutingMode)
+	fmt.Fprintf(os.Stderr, "error: %v \n", errExecutingMode)
 }
 
 // PrintErrorReadingInput is used to handle error logs reading input
 func PrintErrorReadingInput() {
-	fmt.Fprintf(os.Stderr, "error %v \n", errReadingInput)
+	fmt.Fprintf(os.Stderr, "error: %v \n", errReadingInput)
 }
